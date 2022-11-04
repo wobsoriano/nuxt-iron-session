e521b9a45bd1953c292a08d24821021ad5e1ca13
wobsoriano <sorianorobertc@gmail.com>
2022-11-04 08:41:56 -0700

    feat(deps): bump nuxt to 3.0.0-rc.13

--- src/module.ts
+++ src/module.ts
@@ -8,7 +8,7 @@ export default defineNuxtModule<IronSessionOptions>({
     name: 'nuxt-iron-session',
     configKey: 'session',
     compatibility: {
-      nuxt: '^3.0.0-rc.12'
+      nuxt: '^3.0.0-rc.13'
     }
   },
   defaults: {
